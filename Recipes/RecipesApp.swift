3c1ed2cc86b60a526b10204bcf57717fd306dc9c
Liz Goldston <lizgoldston@gmail.com>
2021-04-27 08:32:36 -0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by LizG on 2021-04-27.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
